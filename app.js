ca65259b944c25cc784bc712ee2190047854e144
rohmatsetiawanmta <rohmatsetiawanmta@gmail.com>
2023-07-23 21:18:44 +0700

    Custom Error Middleware

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const categoriesRouter = require('./app/api/v1/categories/router');
 
 const v1 = '/api/v1/cms';
 
+// middleware
+const notFoundMiddleware = require('./app/middlewares/not-found');
+const errorHandlerMiddleware = require('./app/middlewares/handler-error');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -17,5 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(v1, categoriesRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 module.exports = app;
