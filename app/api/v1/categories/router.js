747e74169ced48123f05adfb04c8ae0f13bf5c96
rohmatsetiawanmta <rohmatsetiawanmta@gmail.com>
2023-07-23 19:03:06 +0700

    Model, Controller, and Router for Categories db

--- app/api/v1/categories/router.js
+++ app/api/v1/categories/router.js
@@ -1,10 +1,11 @@
 const express = require('express').Router;
 const router = express();
+const { create, index, find, update, destroy } = require('./controller');
 
-router.get('/categories', (req, res) => {
-  res.status(200).json({
-    message: 'categories',
-  });
-});
+router.get('/categories', index);
+router.post('/categories', create);
+router.get('/categories/:id', find);
+router.put('/categories/:id', update);
+router.delete('/categories/:id', destroy);
 
 module.exports = router;
